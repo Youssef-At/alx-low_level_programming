170ba13f65328e8f7bead7bea0aff8a7723b67b1
Youssef-At <youssefataifa0212@gmail.com>
2023-04-15 15:41:00 -0700

    low_level_programming

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,39 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
+#include "main.h"
 
-int is_valid_number(const char *num) {
-    /* Check if a string represents a valid positive integer. */
-    if (!num || !*num)
-        return 0;
-    while (*num) {
-        if (!isdigit(*num))
-            return 0;
-        if (num[0] == '0' && num[1] != '\0')
-            return 0;
-        num++;
-    }
-    return 1;
-}
+/**
+ * main - multiplies two positive numbers
+ * @argc: n arguments
+ * @argv: args
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+unsigned long mul;
+int i, j;
+	if (argc != 3)
+	{ printf("Error\n");
+	exit(98); }
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{  printf("Error\n");
+			exit(98); }
+		}
 
-int multiply(const char *num1, const char *num2) {
-    /* Multiply two positive integers. */
-    int result = 0;
-    while (*num2) {
-        result = result * 10 + (*num2 - '0') * atoi(num1);
-        num2++;
-    }
-    return result;
-}
-
-int main(int argc, char *argv[]) {
-    /* Parse command-line arguments and perform the multiplication. */
-    if (argc != 3 || !is_valid_number(argv[1]) || !is_valid_number(argv[2])) {
-        fprintf(stderr, "Error\n");
-        return 98;
-    }
-    int result = multiply(argv[1], argv[2]);
-    printf("%d\n", result);
-    return 0;
+	}
+	mul = atol(argv[1]) *  atol(argv[2]);
+	printf("%lu\n", mul);
+return (0);
 }
